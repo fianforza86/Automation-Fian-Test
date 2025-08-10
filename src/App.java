82a120a8872c17afbf7b064c88081de8a34e50d0
Fian Forza <fianforza86@email.com>
2025-08-10 15:00:54 +0700

    Update ci.yml

--- src/App.java
+++ src/App.java
@@ -1,41 +1,38 @@
-
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
-        // Path ke chromedriver (ubah sesuai lokasi kamu)
-        //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        // Setup opsi Chrome agar kompatibel di GitHub Actions (headless mode)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Headless versi baru
+        options.addArguments("--no-sandbox");   // Hindari error sandbox di container
+        options.addArguments("--disable-dev-shm-usage"); // Hindari limit memori
+        options.addArguments("--remote-allow-origins=*"); // Untuk menghindari beberapa error Chrome terbaru
 
-        WebDriver driver = new ChromeDriver();
+        // Inisialisasi driver dengan opsi
+        WebDriver driver = new ChromeDriver(options);
+
+        // Buka URL
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-       driver.findElement(By.id ("autosuggest")).sendKeys("Ind");
-       Thread.sleep(3000);
+        // Interaksi dropdown autocomplete
+        driver.findElement(By.id("autosuggest")).sendKeys("Ind");
+        Thread.sleep(3000);
 
-       List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
-
-
-        for (WebElement option :options)
-        {
-            if(option.getText().equalsIgnoreCase("Indonesia"))
-            {
+        List<WebElement> optionsList = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+        for (WebElement option : optionsList) {
+            if (option.getText().equalsIgnoreCase("Indonesia")) {
                 option.click();
                 break;
             }
         }
 
-        try {
-            Thread.sleep(5000); // Tunggu 5 detik
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(5000); // Tunggu 5 detik agar bisa melihat hasil sebelum close
         driver.quit();
-
     }
 }
